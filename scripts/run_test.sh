d5dd744079eb68798be8358ca5c7f909cb34c740
Jannis Schönleber <joennlae@gmail.com>
2023-05-17 11:50:04 +0200

    lic: add license

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Author:  Jannis Schönleber <janniss@iis.ee.ethz.ch>
+
 cd build && make && cd ..
 svase iguana_padframe_fixture iguana_svase.sv iguana_padframe_fixture.pickle.sv --split
 slang iguana_svase.sv  -Wrange-oob --allow-use-before-declare -Wrange-width-oob -error-limit=4419 -top iguana_padframe__15538910711671852192
