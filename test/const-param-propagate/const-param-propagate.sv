b7666e7df098cd1f28c89900543a8d51b19d8b8c
Philippe Sauter <phsauter@student.ethz.ch>
2023-06-13 16:03:50 +0200

    feat(test): add port-parameter propagation

--- test/const-param-propagate/const-param-propagate.sv
+++ test/const-param-propagate/const-param-propagate.sv
@@ -1,5 +1,7 @@
-module test2 ();
-
+module test2 #(
+	parameter int unsigned PortParam = 0
+) ( );
+	localparam int unsigned Module2Param = PortParam;
 endmodule
 
 
@@ -9,17 +11,18 @@ module test #(
 ) ( );
 	localparam int unsigned TopParam = unsigned'($clog2(ModuleParam));
 
-	test2 i_test2();
+	test2 #(.PortParam(TopParam)) i_test2 ( );
 
 	if (IfElseParam == unsigned'(1)) begin
 		localparam int unsigned GenParam = unsigned'($clog2(ModuleParam));
 	end else begin
 		localparam int unsigned GenParam = unsigned'(ModuleParam);
 
-		for (genvar level = 0; level < GenParam; level++) begin : gen_for_outer
-			localparam int unsigned OuterForParam = 2**level;
-			for (genvar l = 0; l < 2**level; l++) begin : gen_for_inner
-				localparam int unsigned InnerForParam = 2**level-1+l;
+		for (genvar k = 0; k < GenParam; k++) begin : gen_for_outer
+			localparam int unsigned OuterForParam = 2**k;
+
+			for (genvar l = 0; l < 2**k; l++) begin : gen_for_inner
+				localparam int unsigned InnerForParam = 2**k+l;
 			end
 		end
 	end
