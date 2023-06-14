e25c39ae9c995e800d437db31b1daa35d70b1d24
Philippe Sauter <phsauter@student.ethz.ch>
2023-06-14 20:11:36 +0200

    feat(test): add license information

--- test/const-param-propagate/const-param-propagate.sv
+++ test/const-param-propagate/const-param-propagate.sv
@@ -1,3 +1,9 @@
+// Copyright (c) 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Author:  Philippe Sauter <phsauter@student.ethz.ch>
+//
 // Test the following parameter propagation scenarios:
 // - to port of instantiated module
 // - to params in scope of module itself
