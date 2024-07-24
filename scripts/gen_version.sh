e192e390fae9090c20fdb6b4de4863c072eb05b1
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2024-07-24 13:31:34 +0200

    chore: add header with license info

--- scripts/gen_version.sh
+++ scripts/gen_version.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright (c) 2024 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Author:
+# - Philippe Sauter <phsauter@iis.ee.ethz.ch>
 
 # latest tag
 TAG=$(git describe --tags --abbrev=0)
